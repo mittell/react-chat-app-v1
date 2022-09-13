1843296e51d568d6d198ed2ae0a1fc6af6bb92cb
Chris Mittell <mittell@hotmail.co.uk>
2022-09-13 14:36:31 +0900

    feat: added Chat and ChatMessage components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import { auth } from './firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import Chat from './components/Chat';
 
 const styles = {
 	appContainer: `max-w-[728px] mx-auto text-center`,
@@ -17,6 +18,7 @@ function App() {
 		<div className={styles.appContainer}>
 			<section className={styles.sectionContainer}>
 				<Navbar />
+				<Chat />
 			</section>
 		</div>
 	);
---
+++ src/components/Chat.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { db } from '../firebase';
+import ChatMessage from './ChatMessage';
+
+const styles = {
+	main: `flex flex-col p-[10px] relative`,
+};
+
+const Chat = () => {
+	const [messages, setMessages] = useState([]);
+	const scroll = useRef();
+
+	useEffect(() => {
+		const q = query(collection(db, 'messages'), orderBy('timestamp'));
+		const unsubscribe = onSnapshot(q, (querySnapshot) => {
+			let messages = [];
+			querySnapshot.forEach((doc) => {
+				messages.push({ ...doc.data(), id: doc.id });
+			});
+			setMessages(messages);
+		});
+		return () => unsubscribe();
+	}, []);
+
+	return (
+		<>
+			<main className={styles.main}>
+				{messages &&
+					messages.map((message) => (
+						<ChatMessage key={message.id} message={message} />
+					))}
+			</main>
+
+			<span ref={scroll}></span>
+		</>
+	);
+};
+
+export default Chat;
