5c33f7e0ad631a3162984b09a95e977fd1d8ef67
Chris Mittell <mittell@hotmail.co.uk>
2022-09-13 12:52:43 +0900

    feat: added initial styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import React from 'react';
+
+const styles = {
+	appContainer: `max-w-[728px] mx-auto text-center`,
+	sectionContainer: `flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl relative border`,
+};
+
 function App() {
-	return <div></div>;
+	return (
+		<div className={styles.appContainer}>
+			<section className={styles.sectionContainer}>
+				
+			</section>
+		</div>
+	);
 }
 
 export default App;
